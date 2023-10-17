e4551b04dda5f8687d426e87dd111b2472d5dc25
Julio Formiga <julio.formiga@gmail.com>
2023-10-17 12:27:04 +0200

    [libft] strtrim

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julsanti <julsanti@student.42firenze.it>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 17:47:25 by julsanti          #+#    #+#             */
+/*   Updated: 2023/10/16 17:47:25 by julsanti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	size_t	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (*s1 && ft_strrchr(set, s1[len]))
+		len--;
+	return (ft_substr(s1, 0, len + 1));
+}
