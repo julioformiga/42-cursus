ed17a5831969b22c1ac178b1950793551826c722
Julio Formiga <julio.formiga@gmail.com>
2024-03-14 17:23:27 +0100

    [pipex] wip: ancooora

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -26,29 +26,42 @@ int	ft_fork_error(char *msg)
 
 void	pipex(char *file1, char *file2, char *cmd1, char *cmd2)
 {
-	int		fd1;
-	int		fd2;
+	int		fd[2];
 	pid_t	pid;
 
-	fd1 = open(file1, O_RDONLY);
-	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	// dup2(fd1, STDOUT_FILENO);
-	close(fd1);
+	fd[0] = open(file1, O_RDONLY);
+	fd[1] = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipe(fd) == -1)
+		ft_error("Error: pipe failed\n");
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	// close(fd[0]);
+	// close(fd[1]);
 	pid = fork();
 	if (pid == -1)
 		ft_fork_error("Error: fork failed\n");
 	if (pid == 0)
-		ft_printf("This is the child process (pid: %d)\n", getpid());
+	{
+		ft_printf("This is the child process  (pid: %d)\n", getpid());
+	}
 	else
+	{
+		wait(NULL);
 		ft_printf("This is the parent process (pid: %d)\n", getpid());
-	ft_printf("file2: %s\n", file2);
+	}
+	waitpid(pid, NULL, 0);
 	(void)cmd1;
 	(void)cmd2;
 }
 
 int	main(int argc, char *argv[])
 {
-	(void)argv;
+	// argc = 5;
+	// argv[0] = "pipex";
+	// argv[1] = "input.txt";
+	// argv[2] = "cat";
+	// argv[3] = "wc";
+	// argv[4] = "output.txt";
 	if (argc != 5)
 		return (ft_error("Error: wrong number of arguments\n"));
 	ft_printf("input: %s\n", argv[1]);
