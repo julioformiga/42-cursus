5b28e8c33d1ae1d292d86049a529295a64015fc3
Julio Formiga <julio.formiga@gmail.com>
2024-04-16 00:02:46 +0200

    [fdf] refact: mlx window controls

--- fdf/src/ft_mlx_controls.c
+++ fdf/src/ft_mlx_controls.c
@@ -1,36 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   ft_mlx_controls.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/10 16:13:34 by julio.formiga     #+#    #+#             */
-/*   Updated: 2024/04/10 16:13:34 by julio.formiga    ###   ########.fr       */
+/*   Created: 2024/04/14 01:36:09 by julio.formiga     #+#    #+#             */
+/*   Updated: 2024/04/14 01:36:09 by julio.formiga    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-t_env	ft_mlx_create_env(void)
-{
-	t_env	env;
-
-	env.mlx = mlx_init();
-	if (!env.mlx)
-	{
-		ft_putstr_fd("Error\nmlx_init() failed\n", 2);
-		exit(1);
-	}
-	env.win = mlx_new_window(env.mlx, WIN_WIDTH, WIN_HEIGHT, "FdF 42 Firenze");
-	if (!env.win)
-	{
-		free(env.win);
-		ft_putstr_fd("Error\nmlx_new_window() failed\n", 2);
-		exit(1);
-	}
-	return (env);
-}
 
 static int	ft_mlx_mouse(int button, int x, int y, t_env *env)
 {
@@ -47,21 +27,58 @@ void	ft_mlx_hooks(t_env *env)
 		ft_mlx_destroy_window, env);
 }
 
+static void	ft_mlx_create_point(t_env *env)
+{
+	mlx_pixel_put(env->mlx, env->win, env->cursor_x, env->cursor_y, WHITE);
+}
+
+static void	ft_mlx_create_line(t_env *env, t_line line)
+{
+	t_point	l;
+
+	l.x = line.p0.x;
+	l.y = line.p0.y;
+	while (l.x <= line.p1.x)
+	{
+		mlx_pixel_put(env->mlx, env->win, l.x, l.y, line.color);
+		l.x++;
+		l.y++;
+	}
+}
+
 int	ft_mlx_keypress(int keycode, t_env *env)
 {
+	t_line	line;
+
+	ft_mlx_create_line(env, line);
 	if (keycode == XK_Escape || keycode == 'q')
 		ft_mlx_destroy_window(env);
 	if (keycode == XK_Up || keycode == 'w')
-		ft_printf("Keycode: %d\n", keycode);
-	return (0);
-}
-
-int	ft_mlx_destroy_window(t_env *env)
-{
-	mlx_destroy_window(env->mlx, env->win);
-	mlx_destroy_display(env->mlx);
-	free(env->mlx);
-	exit(EXIT_SUCCESS);
+	{
+		line.p0.x = 300;
+		line.p0.y = 300;
+		line.p1.x = 400;
+		line.p1.y = 400;
+		line.color = WHITE;
+		ft_mlx_create_line(env, line);
+		ft_mlx_create_point(env);
+		env->cursor_y -= 10;
+	}
+	if (keycode == XK_Down || keycode == 's')
+	{
+		ft_mlx_create_point(env);
+		env->cursor_y += 10;
+	}
+	if (keycode == XK_Left || keycode == 'a')
+	{
+		ft_mlx_create_point(env);
+		env->cursor_x -= 10;
+	}
+	if (keycode == XK_Right || keycode == 'd')
+	{
+		ft_mlx_create_point(env);
+		env->cursor_x += 10;
+	}
 	return (0);
 }
 
---
+++ fdf/src/ft_mlx_window.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_mlx_window.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/14 01:35:05 by julio.formiga     #+#    #+#             */
+/*   Updated: 2024/04/14 01:35:05 by julio.formiga    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+t_env	ft_mlx_create_env(void)
+{
+	t_env	env;
+
+	env.mlx = mlx_init();
+	if (!env.mlx)
+	{
+		ft_putstr_fd("Error\nmlx_init() failed\n", 2);
+		exit(1);
+	}
+	env.win = mlx_new_window(env.mlx, WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
+	if (!env.win)
+	{
+		free(env.win);
+		ft_putstr_fd("Error\nmlx_new_window() failed\n", 2);
+		exit(1);
+	}
+	env.cursor_x = WIN_WIDTH / 2;
+	env.cursor_y = WIN_HEIGHT / 2;
+	return (env);
+}
+
+int	ft_mlx_destroy_window(t_env *env)
+{
+	mlx_destroy_window(env->mlx, env->win);
+	mlx_destroy_display(env->mlx);
+	free(env->mlx);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
