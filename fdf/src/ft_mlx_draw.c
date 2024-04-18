0b6673d5e90fb0be71bca8a64a06ba3b07ab1595
Julio Formiga <julio.formiga@gmail.com>
2024-04-18 10:42:59 +0200

    [fdf] add: bresenham algorithm (line)

--- fdf/src/ft_mlx_draw.c
+++ fdf/src/ft_mlx_draw.c
@@ -14,18 +14,56 @@
 
 int	ft_mlx_draw_line(t_env *env, t_point begin, t_point end, int color)
 {
-	t_point	delta;
+	t_point	d;
+	t_point	s;
+	int		err;
+	int		e2;
+
+	d.x = abs(end.x - begin.x);
+	d.y = -abs(end.y - begin.y);
+	s.x = -1;
+	if (begin.x < end.x)
+		s.x = 1;
+	s.y = -1;
+	if (begin.y < end.y)
+		s.y = 1;
+	err = d.x + d.y;
+	e2 = 0;
+	while (1)
+	{
+		mlx_pixel_put(env->mlx, env->win, begin.x, begin.y, color);
+		if (begin.x == end.x && begin.y == end.y)
+			break ;
+		e2 = 2 * err;
+		if (e2 >= d.y)
+		{
+			err += d.y;
+			begin.x += s.x;
+		}
+		if (e2 <= d.x)
+		{
+			err += d.x;
+			begin.y += s.y;
+		}
+	}
+	return (0);
+}
+
+int	ft_mlx_draw_line_old(t_env *env, t_point begin, t_point end, int color)
+{
+	double	deltax;
+	double	deltay;
 	int		pixels;
 
-	delta.x = begin.x;
-	delta.y = begin.y;
-	pixels = sqrt((delta.x * delta.x) + (delta.y * delta.y));
+	deltax = begin.x;
+	deltay = begin.y;
+	pixels = sqrt((deltax * deltax) + (deltay * deltay));
 	ft_printf("pixels = %d\n", pixels);
 	while (--pixels)
 	{
-		mlx_pixel_put(env->mlx, env->win, delta.x, delta.y, color);
-		delta.x += (end.x - delta.x) / pixels;
-		delta.y += (end.y - delta.y) / pixels;
+		mlx_pixel_put(env->mlx, env->win, deltax, deltay, color);
+		deltax += (end.x - deltax) / pixels;
+		deltay += (end.y - deltay) / pixels;
 	}
 	return (0);
 }
