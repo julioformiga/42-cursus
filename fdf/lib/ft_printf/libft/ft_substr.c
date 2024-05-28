a56ff1480d6ceada1e721b9aa14ad3bac583934d
Julio Formiga <julio.formiga@gmail.com>
2024-05-28 18:29:24 +0200

    [fdf] chg: improve ft_substr@libft

--- fdf/lib/ft_printf/libft/ft_substr.c
+++ fdf/lib/ft_printf/libft/ft_substr.c
@@ -20,18 +20,15 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (start > ft_strlen(s))
-		return (malloc(1));
+		return (ft_strdup(""));
 	if (len > ft_strlen(s + start))
 		len = ft_strlen(s + start);
-	str = malloc((len + 1) * sizeof(char));
+	str = (char *)malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
+	i = -1;
+	while (i++, i < len)
 		str[i] = s[start + i];
-		i++;
-	}
 	str[i] = 0;
 	return (str);
 }
