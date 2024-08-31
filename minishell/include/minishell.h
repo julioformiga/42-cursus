810a282a03e9b1da640c2b13bd91e66da0b68379
Julio Formiga <julio.formiga@gmail.com>
2024-08-31 06:56:48 +0200

    [minishell] init

---
+++ minishell/include/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 14:35:35 by julio.formiga     #+#    #+#             */
+/*   Updated: 2024/08/30 14:35:35 by julio.formiga    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "ft_printf.h"
+#endif
---
+++ minishell/src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 13:34:41 by julio.formiga     #+#    #+#             */
+/*   Updated: 2024/08/30 13:34:41 by julio.formiga    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	ft_printf("Welcome to minisHELL!\n");
+	return (0);
+}
