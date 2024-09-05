910fdc28a1b6949ef7bbd121bfeefdbd3c4b01f4
Julio Formiga <julio.formiga@gmail.com>
2024-09-05 08:39:59 +0200

    [minishell] add: libs sys/types.h, curses.h & dirent.h

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -15,8 +15,11 @@
 # include "libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
 # include <string.h>
 # include <termios.h>
+# include <curses.h>
+# include <dirent.h>
 
 extern int	g_signal;
 
