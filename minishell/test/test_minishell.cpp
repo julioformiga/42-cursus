02c292028d9b5382c08d9bfdc2af43f63e0891c1
Julio Formiga <julio.formiga@gmail.com>
2024-09-09 15:16:55 +0200

    [minishell] upd: work with neotest-gtest (wip: command line)

--- minishell/test/test_minishell.cpp
+++ minishell/test/test_minishell.cpp
@@ -1,15 +1,17 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <unistd.h>
+#include <libgen.h>
 
 using namespace std;
 
-TEST(minishell, test1) {
+TEST(minishell, dumb_test) {
     cout << "Test 1" << endl;
     ASSERT_TRUE(true);
 }
 
-TEST(minishell, no_memory_leaks) {
-    const char* command = "echo 'exi' | valgrind --leak-check=full --show-leak-kinds=all --suppressions=../readline.supp --error-exitcode=1 ./minishell";
+TEST(minishell, init_mleaks) {
+    const char* command = "echo 'exit' | valgrind --leak-check=full --show-leak-kinds=all --suppressions=readline.supp --error-exitcode=1 bin/minishell";
     int result = system(command);
     ASSERT_EQ(result, 0) << "Memory leak detected or error occurred";
 }
