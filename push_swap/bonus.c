256d8af8bbea3d1782b84b06876f274e1d30913d
Julio Formiga <julio.formiga@gmail.com>
2024-09-23 17:23:16 +0200

    [push_swap] refact: only for run (a ca..coda di cane)

--- push_swap/bonus.c
+++ push_swap/bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "src/push_swap.h"
+#include "includes/push_swap.h"
 
 static int	checker(int check)
 {
--- push_swap/src/sort.c
+++ push_swap/src/sort.c
@@ -39,6 +39,8 @@ void	ft_stack_sort(t_stack **stack_a, t_stack **stack_b)
 		ft_stack_5_sort(stack_a, stack_b);
 	else if (data.len == 6)
 		ft_stack_6_sort(stack_a, stack_b);
+	else if (data.len <= 100)
+		ft_stack_radix_sort(stack_a, stack_b);
 	else
 		ft_stack_radix_sort(stack_a, stack_b);
 }
