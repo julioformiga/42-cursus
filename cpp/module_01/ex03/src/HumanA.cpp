37072f950a821e0a200ed587a477c24a63a4eec5
Julio Formiga <julio.formiga@gmail.com>
2025-05-27 13:03:24 +0200

    [cpp01]: ex03: colors added

--- cpp/module_01/ex03/src/HumanA.cpp
+++ cpp/module_01/ex03/src/HumanA.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 
 HumanA::HumanA(std::string name, Weapon &weapon)
-    : _name(name), _weapon(weapon) {}
+    : _name(name), _weapon(weapon) {
+  std::cout << "\033[1;31m-----============== [ A] =============-----"
+            << std::endl;
+}
 
-HumanA::~HumanA(void) {}
+HumanA::~HumanA(void) { std::cout << "\033[0m" << std::ends; }
 
 void HumanA::attack() const {
   std::cout << _name << " attacks with their " << _weapon.getType()
--- cpp/module_01/ex03/src/HumanB.cpp
+++ cpp/module_01/ex03/src/HumanB.cpp
@@ -1,9 +1,12 @@
 #include "HumanB.hpp"
 #include <iostream>
 
-HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {
+  std::cout << "\033[1;34m-----============== [ B] =============-----"
+            << std::endl;
+}
 
-HumanB::~HumanB(void) {}
+HumanB::~HumanB(void) { std::cout << "\033[0m" << std::ends; }
 
 void HumanB::setWeapon(Weapon &weapon) { _weapon = &weapon; }
 
