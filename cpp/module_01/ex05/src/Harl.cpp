b98e64ef96b767b2bb101dcec475d815003aa8e7
Julio Formiga <julio.formiga@gmail.com>
2025-06-04 23:57:47 +0200

    [cpp01]: ex05: Finished

---
+++ cpp/module_01/ex05/src/Harl.cpp
@@ -0,0 +1,49 @@
+#include "Harl.hpp"
+#include <iostream>
+
+void Harl::debug(void) {
+  std::cout << "\033[1;36m"
+            << "[  DEBUG ]\nI love having extra bacon for my "
+               "7XL-double-cheese-triple-pickle-special-ketchup burger. "
+               "I really do !"
+            << "\033[0m" << std::endl;
+}
+
+void Harl::info(void) {
+  std::cout
+      << "\033[1;34m"
+      << "[  INFO ]\nI cannot believe adding extra bacon costs more money. "
+         "You didn’t put enough bacon in my burger! If you did, I wouldn’t "
+         "be asking for more!"
+      << "\033[0m" << std::endl;
+}
+
+void Harl::warning(void) {
+  std::cout
+      << "\033[1;33m"
+      << "[  WARNING ]\nI think I deserve to have some extra bacon for "
+         "free. I’ve been coming for years, whereas you started working here "
+         "just last month."
+      << "\033[0m" << std::endl;
+}
+
+void Harl::error(void) {
+  std::cout << "\033[1;31m"
+            << "[  ERROR ]\nThis is unacceptable! I want to speak to the "
+               "manager now."
+            << "\033[0m" << std::endl;
+}
+
+void Harl::complain(std::string level) {
+  std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+  void (Harl::*funcs[4])() = {&Harl::debug, &Harl::info, &Harl::warning,
+                              &Harl::error};
+  for (int i = 0; i < 4; ++i) {
+    if (levels[i] == level) {
+      (this->*funcs[i])();
+      return;
+    }
+  }
+  std::cout << "[ Probably complaining about insignificant problems ]"
+            << std::endl;
+}
