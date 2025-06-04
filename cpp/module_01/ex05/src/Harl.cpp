e1bdaafdccd2a826c68d59a85701d3ded1e48554
Julio Formiga <julio.formiga@gmail.com>
2025-06-05 01:40:07 +0200

    [cpp01]: ex05: chore: add "\n"

--- cpp/module_01/ex05/src/Harl.cpp
+++ cpp/module_01/ex05/src/Harl.cpp
@@ -4,17 +4,17 @@
 void Harl::debug(void) {
   std::cout << "\033[1;36m"
             << "[  DEBUG ]\nI love having extra bacon for my "
-               "7XL-double-cheese-triple-pickle-special-ketchup burger. "
-               "I really do !"
+               "7XL-double-cheese-triple-pickle-special-ketchup burger.\n"
+               "I really do !\n"
             << "\033[0m" << std::endl;
 }
 
 void Harl::info(void) {
   std::cout
       << "\033[1;34m"
-      << "[  INFO ]\nI cannot believe adding extra bacon costs more money. "
+      << "[  INFO ]\nI cannot believe adding extra bacon costs more money.\n"
          "You didn’t put enough bacon in my burger! If you did, I wouldn’t "
-         "be asking for more!"
+         "be asking for more!\n"
       << "\033[0m" << std::endl;
 }
 
@@ -22,15 +22,15 @@ void Harl::warning(void) {
   std::cout
       << "\033[1;33m"
       << "[  WARNING ]\nI think I deserve to have some extra bacon for "
-         "free. I’ve been coming for years, whereas you started working here "
-         "just last month."
+         "free.\nI’ve been coming for years, whereas you started working here "
+         "just last month.\n"
       << "\033[0m" << std::endl;
 }
 
 void Harl::error(void) {
   std::cout << "\033[1;31m"
             << "[  ERROR ]\nThis is unacceptable! I want to speak to the "
-               "manager now."
+               "manager now.\n"
             << "\033[0m" << std::endl;
 }
 
